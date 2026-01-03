f24f5dc742db85bfed006d07d5aef909a627a98c
Edson Rodriguez <edson.rod.p19@gmail.com>
2026-01-03 15:34:44 -0800

    Adds and Inits spring boot project with dependencys

---
+++ backend/src/test/java/com/servicedesk/lite/ServicedeskLiteApplicationTests.java
@@ -0,0 +1,12 @@
+package com.servicedesk.lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicedeskLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
