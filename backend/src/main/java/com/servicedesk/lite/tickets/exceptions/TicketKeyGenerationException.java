6d14f6c3ed67c6928188930144e04165ac67c7da
Edson Rodriguez <edson.rod.p19@gmail.com>
2026-03-29 11:55:28 -0700

    feat(exceptions): Add new exception TicketKeyGenerationException for TicketKeyGenerator

---
+++ backend/src/main/java/com/servicedesk/lite/tickets/exceptions/TicketKeyGenerationException.java
@@ -0,0 +1,7 @@
+package com.servicedesk.lite.tickets.exceptions;
+
+public class TicketKeyGenerationException extends RuntimeException {
+  public TicketKeyGenerationException(String message) {
+    super(message);
+  }
+}
